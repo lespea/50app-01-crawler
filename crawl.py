020dc8130de6b7570ab37ff355a88ff9fb7527d1
Adam Lesperance <lespea@gmail.com>
2012-01-22 23:48:50 -0600

    Add command line parsing

--- crawl.py
+++ crawl.py
@@ -4,30 +4,23 @@ Created on 2012-01-22
 @author: Adam Lesperance
 '''
 
+import argparse
 import re
 import lxml.html
 from urllib.parse import urlparse
 
 
-default_base_url = "http://www.cnn.com"
-default_max_level = 2
-default_search_string = re.compile(r"(?i)sopa")
-default_stay_local = True
-
-DEBUG = True
+DEBUG = False
 
 
 def parse(
-          target=default_base_url,
-          level=1,
-          max_level=default_max_level,
-          stay_local=default_stay_local,
-          search_string=default_search_string,
-          parsed_links={}):
+          target, max_level, stay_local, search_string,
+          level=1, parsed_links={}):
     if (level > max_level):
         return parsed_links
 
-    print("Parsing [level %d] - %s" % (level, target))
+    if (DEBUG):
+        print("Parsing [level %d] - %s" % (level, target))
 
     try:
         hostname = urlparse(target)[1]
@@ -43,19 +36,38 @@ def parse(
             if (stay_local and urlparse(href)[1] != hostname):
                 continue
             if (href not in parsed_links):
-                parse(href, level + 1, max_level, stay_local,
-                      search_string, parsed_links)
+                parse(href, max_level, stay_local, search_string,
+                      level + 1, parsed_links)
     except Exception as e:
         print("Couldn't parse the target {}: {!s}".format(target, e))
 
     return parsed_links
 
 
+def get_params():
+    parser = argparse.ArgumentParser(description="Crawl a web page")
+    parser.add_argument('--depth', '-d', dest="depth",
+                        type=int, nargs='?', default=5,
+                        help="How many levels to descend")
+    parser.add_argument('--roam', '-r', dest="local",
+                        action='store_false', default=True,
+                        help="The crawler can go to different hostnames")
+    parser.add_argument('search', help="The string to search for")
+    parser.add_argument('target', help="The website to crawl")
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    links = parse()
+    params = get_params()
+    if (DEBUG):
+        print(params)
+
+    links = parse(params.target, params.depth,
+                  params.local, re.compile(params.search))
+
     print(("Found {:d} links; ".format(len(links)) +
            "the following URLs contained the search string {!s}")
-          .format(default_search_string))
-    for (key, value) in links.items:
+          .format(params.search))
+    for (key, value) in links.items():
         if (value):
             print("\t%s" % key)
